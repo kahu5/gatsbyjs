411b4a8f7c01a2d2397794cc9b16ad02667e41a3
kahu5 <47630569+kahu5@users.noreply.github.com>
2022-04-04 13:23:14 -0400

    Add files via upload

---
+++ src/components/ContactForm2.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+class ContactForm extends React.Component {
+
+  render() {
+    const formStyle = {
+      textAlign: 'left',
+      marginLeft: '30px',
+    }
+    const buttonsStyle = {
+      margin: '1rem',
+      listStyleType: 'none',
+    }
+    const inputStyle = {
+      margin: '2px',
+    }
+    return (
+      <form style={formStyle} className="form" action={this.props.action} method="post">
+      <div className="formRow">
+          <label>Full name</label>
+          <input style={inputStyle} type="text" name="fullname"/></div>
+      <div className="formRow">
+          <label>Email</label>
+          <input style={inputStyle} type="email" name="email"/></div>
+      <div className="formRow">
+          <label>Message</label>
+          <textarea style={inputStyle} name="message" rows="5" cols="30"></textarea></div>
+      <div className="submitRow">
+            <ul className="actions" style={buttonsStyle}>
+            <li>
+              <button type="submit" className="button special">Send</button>
+            </li>
+        </ul></div>
+      </form>
+    );
+  }
+}
+
+export default ContactForm
