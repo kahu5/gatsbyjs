f0dff24f7f7d0b761adbb55dade41d21c45e4b5e
kahu5 <47630569+kahu5@users.noreply.github.com>
2022-04-04 11:18:31 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,24 +7,61 @@ import Seo from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
+    <Seo title="Small Valley Media Homepage" />
+  <h2>Welcome. Our partnership delivers secure and sustainable solutions to achive your business outcomes.</h2> 
+      <StaticImage
+      src="../images/jopwell-sm.webp"
+      width={400}
       quality={95}
-      formats={["auto", "webp", "avif"]}
-      alt="A Gatsby astronaut"
+      formats={["AUTO", "WEBP", "AVIF"]}
+      alt="sustainable and secure work solutions"
+      className="paragraphImage"
       style={{ marginBottom: `1.45rem` }}
     />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
-      <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
-      <Link to="/using-dsg">Go to "Using DSG"</Link>
-    </p>
+  <p>We provide online hosting and communication infrastructure for your web solutions. Use your platform with confidence to advance business outomes now.</p>
+   <p>Integrate your data sources into meaningful reporting you can take action on.</p>
+
+  <ul class="actionList">
+    <li>Augment your team's work with our experise on-demand</li>
+    <li>Receive professional engineering for your custom website</li>
+    <li>Consult and receive solid support for tech issues or on-going maintenance </li>
+  </ul>
+<p>Let the computers compute so people can be personal!</p>
+  <div style={{ clear: "both", display: "grid", gridTemplateColumns: "repeat(3, 1fr)", gridGap: 20 }}>
+    <div style={{backgroundColor: "#bdbdbd",}}>    <StaticImage
+      src="../images/bi_report.webp"
+      width={300}
+      quality={95}
+      formats={["AUTO", "WEBP", "AVIF"]}
+      alt="sustainable and secure work solutions"
+      className="paragraphImage"
+      style={{ marginBottom: `1.45rem` }}
+    />
+    <h4 style={{textAlign: "center",}}>Business Intelligence Reports</h4>
+  </div>
+    <div style={{backgroundColor: "#bdbdbd",}}>    <StaticImage
+      src="../images/email_auth.webp"
+      width={300}
+      quality={95}
+      formats={["AUTO", "WEBP", "AVIF"]}
+      alt="sustainable and secure work solutions"
+      className="paragraphImage"
+      style={{ marginBottom: `1.45rem` }}
+    />
+      <h4 style={{textAlign: "center",}}>Email Authentication</h4>
+  </div>
+    <div style={{backgroundColor: "#bdbdbd",}}>    <StaticImage
+      src="../images/web_host.webp"
+      width={300}
+      quality={95}
+      formats={["AUTO", "WEBP", "AVIF"]}
+      alt="sustainable and secure work solutions"
+      className="paragraphImage"
+      style={{ marginBottom: `1.45rem` }}
+    />
+        <h4 style={{textAlign: "center",}}>Secure Domain Management</h4>
+  </div>
+  </div>
   </Layout>
 )
 
