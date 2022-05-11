bdfc125a00d263e22d56bb11e58510e23d124241
kahu5 <47630569+kahu5@users.noreply.github.com>
2022-05-11 14:00:37 -0400

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,7 +35,7 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
-    // `gatsby-plugin-recaptcha`,
+     `gatsby-plugin-recaptcha`,
     //    {
     //  resolve: `gatsby-plugin-name`,
    //   options: {
